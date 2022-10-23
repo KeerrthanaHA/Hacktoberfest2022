b4cd9b2fbc9142ba0f46138b6e3f880aa77ff6bf
KeerrthanaHA <116490160+KeerrthanaHA@users.noreply.github.com>
2022-10-23 17:07:26 +0530

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,11 @@
+# Creating a Dictionary
+# with Integer Keys
+Dict = {1: 'Collins', 2: 'Mustang', 3: 'Ingland'}
+print("\nDictionary with the use of Integer Keys: ")
+print(Dict)
+
+# Creating a Dictionary
+# with Mixed keys
+Dict = {'Name': 'Collins', 1: [5, 4, 7, 33]}
+print("\nDictionary with the use of Mixed Keys: ")
+print(Dict)
